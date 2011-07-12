b22f322f773d4c1ef565940af2b4013d3b927364
Brian Joseff <Brian.D.P.Joseff@dartmouth.edu>
2011-07-12 13:43:51 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupie::Application.config.secret_token = '3ed81ca300ec602b62fefc0623330022aa04177f14041ed56e9d85f8d349bc5c5a9e651ca8e6221bfb10c10f16f885a1d35cdb0e8f715a9362cd37c2d3042e70'
