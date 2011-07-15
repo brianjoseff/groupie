812a41fa6cf7bd016f31a5fb48d9d379080ff6ee
Brian Joseff <Brian.D.P.Joseff@dartmouth.edu>
2011-07-15 15:44:05 -0700

    fixed routes, got homepage forms set with partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Groupie::Application.routes.draw do
 
+resources :users
+resources :sessions, :only => [:new, :create, :destroy]
+
 match '/signup', :to => 'users#new'
+match '/about', :to => 'pages#about'
+match '/contact', :to => 'pages#contact'
+
 root :to => 'pages#home'
 end
