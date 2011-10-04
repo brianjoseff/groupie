c5c8de15d48e335b7266f8b44196ecd719d605f9
Brian Joseff <Brian.D.P.Joseff@dartmouth.edu>
2011-10-03 22:02:04 -0400

    delete bad random method, but keep good one

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,34 +12,35 @@ class User < ActiveRecord::Base
   has_many :groups_as_member, :through => :memberships, :source => :group, :class_name => "Group", :foreign_key => :user_id
   has_many :groups_as_owner, :class_name => "Group"
   has_many :posts
-  
-  def get_random_items
-    return "joe" unless member_groups == true
-    if member_groups == 1
-      assignments = member_groups.assignments.limit(5)
-      random_items = Post.rand_by_post(assignments)
-    end
-    random_groups = member_groups.sort_by{rand}.slice(0,5)
-    random_items = Array.new
-    i=0
-    return "bob" unless random_groups.empty? == true
-    until i == 10 do
-      random_groups.each do |group|
-        assignments = group.assignments.limit(5)
-        if y = Post.rand_by_post(assignments)
-          random_items << y
-          i+=1
-          if random_items == 5
-            return random_items
-          end
-        else
-          return random_items
-        end
-      end
-    end
-    
-    return random_items
-  end
+
+#bad method, use one below  
+#  def get_random_items
+#    return "joe" unless member_groups == true
+#    if member_groups == 1
+#      assignments = member_groups.assignments.limit(5)
+#      random_items = Post.rand_by_post(assignments)
+#    end
+#    random_groups = member_groups.sort_by{rand}.slice(0,5)
+#    random_items = Array.new
+#    i=0
+#    return "bob" unless random_groups.empty? == true
+#    until i == 10 do
+#      random_groups.each do |group|
+#        assignments = group.assignments.limit(5)
+#        if y = Post.rand_by_post(assignments)
+#          random_items << y
+#          i+=1
+#          if random_items == 5
+#            return random_items
+#          end
+#        else
+#          return random_items
+#        end
+#      end
+#    end
+#    
+#    return random_items
+#  end
   
   def get_some_random_items(groups)
     random_items = Array.new
